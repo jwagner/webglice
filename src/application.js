fcf8af29f842c9b5e99ea3f89471329a82e6e7c0
Jonas Wagner <jonas@29a.ch>
2011-06-04 17:34:58 +0200

    Added water

--- src/application.js
+++ src/application.js
@@ -52,23 +52,43 @@ function prepareScene(){
 
     var vbo = new glUtils.VBO(mesh.grid(GRID)),
         heightmapTexture = new glUtils.Texture2D(resources['gfx/heightmap.png']),
-        grid = new scene.SimpleMesh(vbo),
-        shader = shaderManager.get('heightmap.vertex', 'terrain.frag'),
-        transform = new scene.Transform([grid]),
-        material = new scene.Material(shader, {
-            color: new uniform.Vec3([1, 0, 0]),
-            heightmap: heightmapTexture
-        }, [transform]),
-        lighting = new scene.Uniforms({
+        mountainShader = shaderManager.get('heightmap.vertex', 'terrain.frag'),
+        waterShader = shaderManager.get('transform.vertex', 'color.frag'),
+        transform, waterTransform;
+        
+    var camera = new scene.Camera([
+        new scene.Uniforms({
             skyColor: new uniform.Vec3([0.2, 0.3, 0.35]),
             groundColor: new uniform.Vec3([0.05, 0.1, 0.3]),
             sunColor: new uniform.Vec3([0.7, 0.6, 0.75]),
             sunDirection: new uniform.Vec3([0.577, 0.577, 0.577]),
-        }, [material]),
-        camera = new scene.Camera([lighting]);
+        }, [
+            new scene.Material(mountainShader, {
+                heightmap: heightmapTexture
+            }, [
+                transform = new scene.Transform([
+                    new scene.SimpleMesh(vbo),
+                ])
+            ]),
+            new scene.Material(waterShader,{
+                color: new uniform.Vec3([0, 0, 1])
+            }, [
+                waterTransform = new scene.Transform([
+                    new scene.SimpleMesh(vbo)
+                ])
+            ])
+        ]),
+    ]);
 
-    mat4.translate(transform.matrix, [-0.5*GRID, -80, -0.5*GRID]);
+    camera.position[1] = 30;
+
+    mat4.translate(transform.matrix, [-0.5*GRID, -50, -0.5*GRID]);
     mat4.scale(transform.matrix, [GRID, 100, GRID]);
+
+
+    mat4.translate(waterTransform.matrix, [-10*GRID, 0, -10*GRID]);
+    mat4.scale(waterTransform.matrix, [GRID*20, 100, GRID*20]);
+
     sceneGraph.root.append(camera);
 
     gl.clearColor(0.4, 0.6, 1.0, 1.0);
